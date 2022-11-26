5d3cb9b1e006ffbdc68ba6fae4f06fbf26f8d58b
Larionova Uliana <ulatlene@mail.ru>
2022-11-26 16:33:02 +0700

    обновление

--- gui.py
+++ gui.py
@@ -125,14 +125,177 @@ model.eval()
 def generate():
     text_box_out.delete("1.0", tk.END)
     #text_box_out.insert("1.0", "dfsdfsdf")
-    text_box_out.insert("1.0", evaluate(
+    text = text_box_in.get("1.0", tk.END)
+
+    if text.find("олот"):
+        Z = "золот"
+    else: 
+        Z = "серебр"
+
+    if text.find("ерьг"):
+        S = "серьги"
+        K = "серьги"
+        k = "украшение"
+    else: 
+        S = "кольцо"
+        K = "кольца"
+        k = "кольцо"
+
+    if text.find("квама"):
+        J = "аквамарины"
+        j = "аквамарин"
+        t = "аквамарином"
+    elif text.find("етис"):
+        J = "аметитсты"
+        j = "аметист"
+        t = "аметистом"
+    elif text.find("етри"):
+        J = "аметриныы"
+        j = "аметрин"
+        t = "аметрином"
+    elif text.find("рюз"):
+        J = "бирюзы"
+        j = "бирюза"
+        t = "бирюзой"
+    elif text.find("иллиан"):
+        J = "бриллианты"
+        j = "бриллиант"
+        t = "бриллиантом"
+    elif text.find("орн"):
+        J = "горные хрустали"
+        j = "горный хрусталь"
+        t = "горным хрусталём"
+    elif text.find("ранат"):
+        J = "гранаты"
+        j = "гранат"
+        t = "гранатом"
+    elif text.find("емчу"):
+        J = "жемчуга"
+        j = "жемчуг"
+        t = "жемчугом"
+    elif text.find("зумру"):
+        J = "изумруды"
+        j = "изумруд"
+        t = "изумрудом"
+    elif text.find("олит"):
+        J = "иолиты"
+        j = "иолит"
+        t = "иолитом"
+    elif text.find("варц"):
+        J = "кварцы"
+        j = "кварц"
+        t = "кварцом"
+    elif text.find("орал"):
+        J = "кораллы"
+        j = "коралл"
+        t = "кораллом"
+    elif text.find("орунд"):
+        J = "корунды"
+        j = "корунд"
+        t = "корундом"
+    elif text.find("алахи"):
+        J = "малахиты"
+        j = "малахит"
+        t = "малахиом"
+    elif text.find("унн"):
+        J = "лунные камни"
+        j = "лунный камень"
+        t = "лунным камнем"
+    elif text.find("органи"):
+        J = "морганиты"
+        j = "морганит"
+        t = "морганиом"
+    elif text.find("никс"):
+        J = "ониксы"
+        j = "оникс"
+        t = "ониксом"
+    elif text.find("пал"):
+        J = "опалы"
+        j = "опал"
+        t = "опалом"
+    elif text.find("ерламут"):
+        J = "перламутры"
+        j = "перламутр"
+        t = "перламутром"
+    elif text.find("разиоли"):
+        J = "празиолиты"
+        j = "празиолит"
+        t = "празиолитом"
+    elif text.find("рени"):
+        J = "прениты"
+        j = "пренит"
+        t = "пренитом"
+    elif text.find("убин"):
+        J = "рубины"
+        j = "рубин"
+        t = "рубином"
+    elif text.find("апфи"):
+        J = "сапфиры"
+        j = "сапфир"
+        t = "сапфиом"
+    elif text.find("анзани"):
+        J = "танзаниты"
+        j = "танзанит"
+        t = "танзанитом"
+    elif text.find("опаз"):
+        J = "топазы"
+        j = "топаз"
+        t = "топазом"
+    elif text.find("урмали"):
+        J = "турмалины"
+        j = "турмалин"
+        t = "турмалином"
+    elif text.find("иниф"):
+        J = "финифты"
+        j = "финифт"
+        t = "финифтом"
+    elif text.find("алцедо"):
+        J = "халцедоны"
+        j = "халцедон"
+        t = "халцедоном"
+    elif text.find("ризоли"):
+        J = "хризолиты"
+        j = "хризолит"
+        t = "хризолитом"
+    elif text.find("итрин"):
+        J = "цитрины"
+        j = "цитрин"
+        t = "цитрином"
+    elif text.find("пинел"):
+        J = "шпинелями"
+        j = "шпинель"
+        t = "шпинелью"
+    elif text.find("маль"):
+        J = "эмали"
+        j = "эмаль"
+        t = "эмалью"
+    elif text.find("нтар"):
+        J = "янтари"
+        j = "янтарь"
+        t = "янтарью"
+    else: 
+        J = "сияния"
+        j = "блеск"
+        t = "изяществом"
+        #model.load_state_dict(torch.load("model_no_kamen.pt"))
+        #model.eval()
+
+    s = evaluate(
         model, 
         char_to_idx, 
         idx_to_char, 
         temp=0.3, 
         prediction_len=200, 
-        start_text=text_box_in.get("1.0", tk.END)
+        start_text=text
         )
+    s=s.replace("J",J)
+    s=s.replace("j",j)
+    s=s.replace("t",t)
+    s=s.replace("Z",Z)
+    s=s.replace("S",S)
+    s=s.replace("K",K)
+    s=s.replace("k",k)
+    text_box_out.insert("1.0", s
     )
     
 
